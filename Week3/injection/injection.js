5e421ded96e1137623a87b2b54d007280503389b
Mahmoud Badran <mahmoudbd@gmail.com>
2020-04-07 03:17:07 +0200

    Updated-Injection-Normalization

--- Week3/injection/injection.js
+++ Week3/injection/injection.js
@@ -1,27 +1,13 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'hyfuser',
-	password: 'hyfpassword',
-	database: 'world'
-});
-
-connection.connect();
+// 1- get all the info if inserted into name {countryName} or 1=1.   .
+// 2- drop table {name of the table }; delete the table 5555 or 1=1;  .
 
 function getPopulation(Country, name, code, cb) {
-	connection.query(
-		`SELECT Population FROM ${Country} WHERE Name = +
-			${connection.escape(name)}and code = +
-			${connection.escape(code)}`,
-		function(err, result) {
-			if (err) cb(err);
-			if (result.length == 0) cb(new Error('Not found'));
-			cb(null, result[0].name);
-		}
-	);
+	connection.query(`SELECT Population FROM ?? WHERE Name = ?AND code = ?`, [ Country, name, code ], function(
+		err,
+		result
+	) {
+		if (err) cb(err);
+		if (result.length == 0) cb(new Error('Not found'));
+		cb(null, result[0].name);
+	});
 }
-
-getPopulation('Country', 'Netherlands', 'NLD', (err, result) => {
-	if (err) console.error(err);
-	else console.log(result);
-});
